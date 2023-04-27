daaee99b7d6bdfd9eca86b04980175333ec87b9e
Minh1327 <125230676+Minh1327@users.noreply.github.com>
2023-04-27 11:03:15 +0700

    Create schema branch

--- schema_tables.sql
+++ schema_tables.sql
@@ -3,5 +3,51 @@ CREATE DATABASE mycompany;
 -- NOTE: After create the database named `mycompany`, you must change the active database to `mycompany` by clicking the DB name in the status bar before running the next queries.
 
 CREATE SCHEMA HR;
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex text CHECK (sex in ('F', 'M')),
+    hire_date DATE
+);
 
+CREATE TABLE departments (
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
+);
 
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR (6) NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+    CONSTRAINT PK_dept_emp PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+    CONSTRAINT PK_dept_manager PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(6) PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+-- Update table employees with foreign key title_id
+ALTER TABLE employees 
+ADD CONSTRAINT emp_title_id 
+FOREIGN KEY (emp_title_id) 
+REFERENCES titles (title_id);
+
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT,
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+    CONSTRAINT PK_salaries PRIMARY KEY (emp_no, salary)
+);
