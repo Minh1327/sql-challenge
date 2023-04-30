70e5662b759ddf051ba286be51c933b6e49e76f0
Minh1327 <125230676+Minh1327@users.noreply.github.com>
2023-04-30 11:47:43 +0700

    Update datatype of tables

--- schema_tables.sql
+++ schema_tables.sql
@@ -1,33 +1,32 @@
-CREATE DATABASE mycompany;
-
--- NOTE: After create the database named `mycompany`, you must change the active database to `mycompany` by clicking the DB name in the status bar before running the next queries.
-
+-- Create a new schema named HR in postgres default database.
 CREATE SCHEMA HR;
+
+
 CREATE TABLE hr.employees (
     emp_no INT PRIMARY KEY,
-    emp_title_id VARCHAR,
+    emp_title_id VARCHAR(5),
     birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     sex text CHECK (sex in ('F', 'M')),
     hire_date DATE
 );
 
 CREATE TABLE hr.departments (
-    dept_no VARCHAR PRIMARY KEY,
-    dept_name VARCHAR NOT NULL
+    dept_no VARCHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE hr.dept_emp (
     emp_no INT NOT NULL,
-    dept_no VARCHAR (6) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     FOREIGN KEY(emp_no) REFERENCES hr.employees(emp_no),
     FOREIGN KEY(dept_no) REFERENCES hr.departments(dept_no),
     CONSTRAINT PK_dept_emp PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE hr.dept_manager (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     FOREIGN KEY(dept_no) REFERENCES hr.departments(dept_no),
     FOREIGN KEY(emp_no) REFERENCES hr.employees(emp_no),
@@ -35,8 +34,8 @@ CREATE TABLE hr.dept_manager (
 );
 
 CREATE TABLE hr.titles (
-    title_id VARCHAR(6) PRIMARY KEY,
-    title VARCHAR NOT NULL
+    title_id VARCHAR(5) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
 );
 
 -- Update table employees with foreign key title_id
@@ -51,3 +50,40 @@ CREATE TABLE hr.salaries (
     FOREIGN KEY(emp_no) REFERENCES hr.employees(emp_no),
     CONSTRAINT PK_salaries PRIMARY KEY (emp_no, salary)
 );
+
+-- Import data from CSV files to Postgres table
+
+
+COPY hr.titles(title_id, title)
+FROM 'D:\Monash bootcamp\Git\sql-challenge\data\titles.csv'
+DELIMITER ','
+CSV HEADER;
+
+SET datestyle TO postgres, mdy;
+
+COPY hr.employees(emp_no, emp_title_id, birth_date,first_name,last_name,sex,hire_date)
+FROM 'D:\Monash bootcamp\Git\sql-challenge\data\employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY hr.salaries(emp_no, salary)
+FROM 'D:\Monash bootcamp\Git\sql-challenge\data\salaries.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY hr.departments(dept_no, dept_name)
+FROM 'D:\Monash bootcamp\Git\sql-challenge\data\departments.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY hr.dept_emp(emp_no, dept_no)
+FROM 'D:\Monash bootcamp\Git\sql-challenge\data\dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY hr.dept_manager(dept_no, emp_no)
+FROM 'D:\Monash bootcamp\Git\sql-challenge\data\dept_manager.csv'
+DELIMITER ','
+CSV HEADER;
+
+
